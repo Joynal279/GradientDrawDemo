4422014bc79b796bb13732a7d70009cbc93a67eb
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-27 21:19:18 +0600

    Initial Commit

---
+++ GradientDrawDemo/GradientDrawDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientDrawDemoApp.swift
+//  GradientDrawDemo
+//
+//  Created by JOYNAL ABEDIN on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GradientDrawDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
