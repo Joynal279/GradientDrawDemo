c97b6b70f9f2d12cf3f30105cdc297d3206702fd
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-27 21:32:35 +0600

    draw gradient colors

--- GradientDrawDemo/ContentView.swift
+++ GradientDrawDemo/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colors = Gradient(colors: [Color.red, Color.yellow, Color.green, Color.blue, Color.purple])
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle()
+                .fill(RadialGradient(gradient: colors, center: .center, startRadius: 0, endRadius: 300))
+            Circle()
+                .fill(AngularGradient(gradient: colors, center: .center))
+            Rectangle()
+                .fill(LinearGradient(gradient: colors, startPoint: .topLeading, endPoint: .bottomTrailing))
+            
+            MyShape()
+                .fill(RadialGradient(gradient: colors, center: .center, startRadius: 0, endRadius: 300))
+                .background(LinearGradient(gradient: Gradient(colors: [Color.white, Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 310, height: 300)
         }
         .padding()
+    }
+}
+
+//Custom Shape Gradient
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY), control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
 
